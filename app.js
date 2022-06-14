d3c38375881e0ca2f02e09ed8eab98ad01a2e7ca
MagicSoft <magicsoft.uz@gmail.com>
2022-06-14 19:19:50 +0500

    Handling GET requests

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'This server is working!',
+//     data: 'Bu yerda json chiqishi kerak edi',
+//   });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'This server is working!',
-    data: 'Bu yerda json chiqishi kerak edi',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
