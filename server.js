caafcf1f8859c0ba53380b6cbb660b3f3aea16b5
MagicSoft <magicsoft.uz@gmail.com>
2022-06-15 13:04:21 +0500

    Enviroment variables

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
+
+console.log('PORT:' + process.env.PORT);
 const port = 8000;
 app.listen(port, () => {
   console.log(`Server running on ${port} port...`);
