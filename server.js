643babbec60b15a9c76146e6223adf9e6647731b
MagicSoft <magicsoft.uz@gmail.com>
2022-06-16 14:06:44 +0500

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {}).then(() => {
+  console.log('DB connected');
+});
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true],
+  },
+  price: Number,
+  rating: Number,
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 console.log('PORT:' + process.env.PORT);
 const port = 8000;
