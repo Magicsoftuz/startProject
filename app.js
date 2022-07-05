d2a7b0ccf4f9ca49c2308faff7d2d8219c794734
MagicSoft <magicsoft.uz@gmail.com>
2022-07-05 14:00:48 +0500

    Remove fs module

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
