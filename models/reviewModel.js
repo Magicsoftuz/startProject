04025fff71a4d946b14aac97da602730c818bf38
MagicSoft <magicsoft.uz@gmail.com>
2022-07-10 10:46:14 +0500

    Modeling Reviews: Parent Referencing 154

---
+++ models/reviewModel.js
@@ -0,0 +1,34 @@
+// review /  rating / createdAt / ref to tour / ref to user
+
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    minlength: [2, 'You enter more words!'],
+    required: [true, 'You should not empty'],
+  },
+  rating: {
+    type: Number,
+    min: [1, 'You enter at least 1 rating'],
+    max: [5, 'You enter maximum 5 rating'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  tourId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'tours',
+    required: [true, 'Review must belong to any tour'],
+  },
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'users',
+    required: [true, 'Review must belong to any user'],
+  },
+});
+
+const Review = mongoose.model('reviews', reviewSchema);
+
+module.exports = Review;
