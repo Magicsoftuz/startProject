aa017f4cacfa70220807b338ec9fe8897dfa055b
MagicSoft <magicsoft.uz@gmail.com>
2022-07-11 10:31:23 +0500

    preventing duplicate review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -3,42 +3,46 @@
 const mongoose = require('mongoose');
 const Tour = require('./tourModel');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    minlength: [2, 'You enter more words!'],
-    required: [true, 'You should not empty'],
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      minlength: [2, 'You enter more words!'],
+      required: [true, 'You should not empty'],
+    },
+    rating: {
+      type: Number,
+      min: [1, 'You enter at least 1 rating'],
+      max: [5, 'You enter maximum 5 rating'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'tours',
+      required: [true, 'Review must belong to any tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'users',
+      required: [true, 'Review must belong to any user'],
+    },
   },
-  rating: {
-    type: Number,
-    min: [1, 'You enter at least 1 rating'],
-    max: [5, 'You enter maximum 5 rating'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  tour: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'tours',
-    required: [true, 'Review must belong to any tour'],
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'users',
-    required: [true, 'Review must belong to any user'],
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name photo',
   });
-  // .populate({
-  //   path: 'tour',
-  //   select: 'name -guides',
-  // });
   next();
 });
 
