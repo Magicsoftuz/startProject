43029422c7c2f57bb7b1008f5b7068e2334df0a9
MagicSoft <magicsoft.uz@gmail.com>
2022-07-11 17:59:39 +0500

    Build overview 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,15 @@
-const getAllTours = (req, res) => {
+const catchErrorAsync = require('../utility/catchAsync');
+const Tour = require('./../models/tourModel');
+
+const getAllTours = catchErrorAsync(async (req, res) => {
+  // 1) Get all tours
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours: tours,
   });
-};
+});
 
 const getOneTour = (req, res) => {
   res.status(200).render('tour', {
